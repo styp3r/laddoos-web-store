70ab1c71b164d02b425cfdc4c374fc19bb880726
Navjyot Jaiswal <stypertypes@gmail.com>
2022-03-15 17:45:22 +0530

    Creates react-router-dom integration by adding 3 links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home.js';
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+import ErrorPage from './components/ErrorPage.js';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <div className = "header"></div>
-      <div className = "navbar"></div>
-      <div id = "hero"></div>
+    <BrowserRouter>
+    <div className = "header"></div>
+    <div className = "navbar">
+      <Link to = "/">Home</Link>
+      <Link to = "about">About</Link>
+      <Link to = "contact">Contact</Link>
+    </div>
+      <Routes>
+        <Route path = "/" index element = {<Home />}/>
+        <Route path = "about" element = {<About />}/>
+        <Route path = "contact" element = {<Contact />}/>
+        <Route path = "*" element = {<ErrorPage />}/> 
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
